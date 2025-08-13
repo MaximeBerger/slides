91e47ceb582c4140aa30910ac6bc05d5dd72129f
MaximeBerger <104891132+MaximeBerger@users.noreply.github.com>
2025-08-13 11:29:47 +0200

    changé dimension dans json

--- plugin/config.js
+++ plugin/config.js
@@ -24,8 +24,17 @@ Reveal.initialize({
     },
     chalkboard: {
         src: 'chalkboard.json',
-        chalkWidth: 3,
+        chalkWidth: 2,
         chalkEffect: 0.0001,
+        chalks: [
+                { color: 'rgba(255,255,255,1)', cursor: 'url(' + path + 'img/chalk-white.png), auto'},
+                { color: 'rgba(96, 154, 244, 1)', cursor: 'url(' + path + 'img/chalk-blue.png), auto'},
+                { color: 'rgba(237, 20, 28, 1)', cursor: 'url(' + path + 'img/chalk-red.png), auto'},
+                { color: 'rgba(20, 237, 28, 1)', cursor: 'url(' + path + 'img/chalk-green.png), auto'},
+                { color: 'rgba(220, 133, 41, 1)', cursor: 'url(' + path + 'img/chalk-orange.png), auto'},
+                { color: 'rgba(220,0,220, 1)', cursor: 'url(' + path + 'img/chalk-purple.png), auto'},
+                { color: 'rgba(255,220,0, 1)', cursor: 'url(' + path + 'img/chalk-yellow.png), auto'}
+        ]
     },  
     mathjax2: {
         config: 'TeX-AMS_HTML-full',
