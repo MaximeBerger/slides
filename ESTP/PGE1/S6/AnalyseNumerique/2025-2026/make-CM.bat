98f0ce742afbc3674d617dcbc97af291f49811fe
MaximeBerger <104891132+MaximeBerger@users.noreply.github.com>
2026-04-01 14:43:09 +0200

    k

--- ESTP/PGE1/S6/AnalyseNumerique/2025-2026/make-CM.bat
+++ ESTP/PGE1/S6/AnalyseNumerique/2025-2026/make-CM.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set "TEXINPUTS=c:\Users\mberge04\OneDrive - association E.S.T.P\Documents\Projets\slides\ESTP\Config;%TEXINPUTS%"
-set "BASE=CM2"
+set "BASE=TD1"
 
 echo Compilation du corrige...
 xelatex -jobname=corrige "%BASE%.tex"
